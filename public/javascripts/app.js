5ebf84154c48f9c52128fe5173846cae5fbd900a
alina <hodyna.alina@gmail.com>
2021-02-04 11:11:29 +0200

    implemented user edit features

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,7 @@
+window.addEventListener('load', function () {
+    printUsersList()
+})
+
 function printUsersList() {
     fetch('/users')
         .then((response) => {
@@ -12,18 +16,16 @@ function printUsersList() {
                 let userComponent = document.createElement("div");
                 userComponent.innerText = data[i].name;
                 usersList.appendChild(userComponent);
-                
+
                 let btnDelete = document.createElement("button");
                 btnDelete.innerText = "Delete";
-            
                 userComponent.appendChild(btnDelete);
-                
-                btnDelete.addEventListener('click', () => { deleteUser(data[i].id) });  
+                btnDelete.addEventListener('click', () => { deleteUser(data[i].id) });
 
-                let bttnEdit = document.createElement("button");
-                bttnEdit.innerText = "Edit";
-                // btnEdit.id = data[i].id;
-                userComponent.appendChild(bttnEdit);
+                let btnEdit = document.createElement("button");
+                btnEdit.innerText = "Edit";
+                userComponent.appendChild(btnEdit);
+                btnEdit.addEventListener('click', () => { openEditPopup(data[i].id, data[i].name, data[i].email) })
             }
 
         });
@@ -56,7 +58,6 @@ function sendData() {
             })
             .then(resp => {
                 if (resp.ok) {
-                    console.log(123)
                     userNameInput.value = '';
                     userEmailInput.value = '';
                     printUsersList()
@@ -68,8 +69,65 @@ function sendData() {
     }
 }
 
-function deleteUser(id){
+function deleteUser(id) {
     fetch(`/users/${id}`, {
         method: 'DELETE',
     }).then(printUsersList())
+}
+
+// popup for edit section
+
+function openEditPopup(id, name, email) {
+    const generalSectionNode = document.getElementById('generalSection')
+
+    const modalBoxNode = document.createElement('div');
+    modalBoxNode.className = 'modal';
+    modalBoxNode.id = 'modalBox'
+    modalBoxNode.style.display = 'block';
+    generalSectionNode.appendChild(modalBoxNode)
+
+    const modalContentNode = modalBoxNode.appendChild(document.createElement('div'))
+    modalContentNode.className = 'modal-content'
+    modalContentNode.id = 'modalContent'
+
+    const popupTitleNode = modalContentNode.appendChild(document.createElement('h3'))
+    popupTitleNode.innerText = 'Edit section'
+
+    const nameInput = document.createElement('input')
+    nameInput.value = name;
+    modalContentNode.appendChild(nameInput)
+
+    const emailInput = document.createElement('input')
+    emailInput.value = email;
+    modalContentNode.appendChild(emailInput)
+
+    let btnSubmit = document.createElement("button");
+    btnSubmit.innerText = "Submit";
+    modalContentNode.appendChild(btnSubmit)
+    btnSubmit.addEventListener('click', () => { submitEditForm(nameInput.value,emailInput.value, id)})
+
+    let btnExit = document.createElement("button");
+    btnExit.innerText = "Exit";
+    modalContentNode.appendChild(btnExit);
+    btnExit.addEventListener('click', () => {document.getElementById('modalBox').remove()} )
+    
+}
+
+function submitEditForm(name, email, id){
+
+    let userData = {
+        name: name,
+        email: email,
+        id: id,
+    }
+
+    fetch(`/users/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify(userData),
+        headers: { 'Content-Type': 'application/json' }
+    }).then(
+        
+        document.getElementById('modalBox').style.display = 'none',
+        document.getElementById('modalBox').remove(),
+        printUsersList())
 }
